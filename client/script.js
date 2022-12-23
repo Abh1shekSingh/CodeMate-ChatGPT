660bd2832b49fa266de82f844e5fa8ebb321cab4
Abhishek Singh <71981463+Abh1shekSingh@users.noreply.github.com>
2022-12-23 21:06:11 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server
 
-  const response = await fetch('https://codemate.onrender.com/', {
+  const response = await fetch('https://codemate.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
